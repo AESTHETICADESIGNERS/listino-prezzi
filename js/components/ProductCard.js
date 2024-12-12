fec65df1a432bde3d3f22b3dd3399aea34308847
SimoTheGamer <74712999+SimoTheGamer@users.noreply.github.com>
2024-12-12 18:38:53 +0100

    aggiornato con nuove funzionalità
    modal prodotti

---
+++ js/components/ProductCard.js
@@ -0,0 +1,19 @@
+class ProductCard {
+    constructor({id, name, description, price, category}) {
+        this.template =
+        `<div class="product flex flex-col p-4 justify-between bg-slate-900 rounded-md border-2 border-gray-500">
+            <b>{name}</b>
+            <p class="text-gray-300">{description}</p>
+            <p class="text-gray-300 text-lg">{price}€</p>
+            <button productid={productId} class="productInfo bg-gray-600 p-2 rounded-lg text-white font-medium">Informazioni</button>
+        </div>`;
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+    }
+    render() {
+        return this.template.replace("{productId}", this.id).replace("{name}", this.name).replace("{description}", this.description || "Nessuna descrizione presente").replace("{price}", this.price);
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,89 @@
+function filterByCategory(category) {
+    if (!category) {
+        productsDiv.innerHTML = productsCards.map(pc => pc.render()).join("");
+        return;
+    }
+    productsDiv.innerHTML = productsCards.filter(pc => pc.product.category === category).map(pc => pc.render()).join("");
+}
+
+function filterByName(name) {
+    if (!name) {
+        productsDiv.innerHTML = productsCards.map(pc => pc.render()).join("");
+    } else {
+        productsDiv.innerHTML = productsCards.filter(pc => pc.name.toLowerCase().includes(name.toLowerCase())).map(pc => pc.render()).join("");
+    }
+    addEventListenerProductsInfoButtons();
+}
+
+const randomID = (len) => {
+    let id = "";
+    for (let i = 0; i < len; i++) {
+        const charCode = 65 + Math.floor(Math.random() * (90 - 65));// 65 - 90
+        id += String.fromCharCode(charCode);
+    }
+    return id;
+}
+
+const addEventListenerProductsInfoButtons = () => {
+    // Aggiungo l'evento on click sul bottone info per ogni prodotto
+    const productsInfoButtons = Array.from(document.getElementsByClassName("productInfo"));
+    productsInfoButtons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            const productId = e.target.getAttribute("productid");
+            const product = productsList.find(product => product.id == productId);
+
+            const productModal = new ProductModal(product);
+
+            productModalDiv.innerHTML = productModal.render();
+
+            // Aggiungo l'azione close al bottone con la X
+            const modalCloseButton = document.getElementById("closeProductModal");
+
+            modalCloseButton.addEventListener("click", (e) => {
+                productModalDiv.innerHTML = "";
+            });
+        });
+    });
+}
+/*
+    PRODUCTS CARDS
+*/
+
+const productsList = [
+    { "id": "IEHKOOBLXC", "name": "Logo personalizzato", "description": "", "price": "Da 2 a 10", "category": "grafiche" },
+    { "id": "YKSOWLXJKA", "name": "Banner Personalizzato", "description": "", "price": "0", "category": "grafiche" },
+    { "id": "ODNODKADMD", "name": "Server Discord", "description": "", "price": "0", "category": "grafiche" },
+    { "id": "PVBEIGBFUW", "name": "Twitch Banner", "description": "", "price": "0", "category": "grafiche" },
+    { "id": "WUKNMTDIVH", "name": "Emoji", "description": "", "price": "0", "category": "grafiche" },
+    { "id": "OHRFLLGXKN", "name": "Facecam Banner", "description": "", "price": "0", "category": "grafiche" },
+    { "id": "WMBLWCSXGN", "name": "Info panel Twitch", "description": "", "price": "0", "category": "grafiche" },
+    { "id": "AJRHETLJGX", "name": "Youtube icon", "description": "", "price": "0", "category": "grafiche" },
+    { "id": "YLUXNGYNOP", "name": "Youtube Banner", "description": "", "price": "0", "category": "grafiche" }
+];
+
+const productsCards = productsList.map((product) => new ProductCard(product));
+
+const productsDiv = document.getElementById("products");
+
+const searchProducts = document.getElementById("searchproducts");
+searchProducts.addEventListener("input", (e) => {
+    filterByName(e.target.value);
+});
+
+productsDiv.innerHTML = productsCards.map(pc => pc.render()).join("");
+
+/*
+    PRODUCTS CARDS
+*/
+
+/*
+    PRODUCTS MODAL
+*/
+
+const productModalDiv = document.getElementById("productModalDiv");
+
+addEventListenerProductsInfoButtons();
+
+/*
+    PRODUCTS MODAL
+*/
