72546b2c4024bfc5bc6ff2ec52b560d6f1ab0bb9
SimoTheGamer <simone81mariuzzo@gmail.com>
2025-01-10 20:37:14 +0100

    sistemo bug view port

--- docs/js/components/dynamo-navbar.js
+++ docs/js/components/dynamo-navbar.js
@@ -2,7 +2,7 @@ class DynamoNavbar extends HTMLElement {
     constructor() {
         super();
         this.innerHTML = `
-        <nav class="bg-[#414040] h-[60px] w-screen flex items-center justify-center sticky top-0">
+        <div class="bg-[#2F2F2F] h-[60px] w-full flex items-center justify-center sticky top-0">
             <div class="h-[50px] w-[150px] md:ml-[60px]"><img src="./images/logo+scritta.png"></div>
             <ul class="hidden md:flex mx-auto text-white font-bold uppercase gap-[35px]">
                 <li><a class="" href="#home">home</a></li>
@@ -10,7 +10,7 @@ class DynamoNavbar extends HTMLElement {
                 <li><a class="" href="#scripts">scripts</a></li>
                 <li><a class="" href="#offerte">offerte</a></li>
             </ul>
-        </nav>
+        </div>
         `;
         this.activatedLinkColor = "text-[#ff6900]";
         this.links = Array.from(this.getElementsByTagName("li"));
@@ -22,7 +22,6 @@ class DynamoNavbar extends HTMLElement {
                 this.activateLink(link.innerText);
             });
         })
-        console.log(this.links[0].innerText)
     }
     activateLink(name) {
         const linkToActivate = this.links.find(link => link.innerText.toLowerCase() === name.toLowerCase());
--- docs/js/components/simple-card.js
+++ docs/js/components/simple-card.js
@@ -1,9 +1,22 @@
 class SimpleCard extends HTMLElement {
     constructor() {
         super();
+        console.log(this.getAttribute("text"));
         this.innerHTML = `
-        
-        `;
+            <div class="p-4 border-2 bg-[#2F2F2F] border-white rounded-md">
+                <div class=""></div>
+                <h2 class="font-bold">
+                ${this.getAttribute("text") ? this.getAttribute("text")
+                    .replace("{coloredtext}", `<span style="color: ${this.getAttribute("color")}">${this.getAttribute("coloredtext")}</span>`) : ""}
+                </h2>
+                <div class="text-[#c7c7c7]">
+                    <p>Lorem ipsum dolor sit amet,</p>
+                    <p>consectetur adipisicing elit.</p>
+                    <p>Assumenda eligendi dolor</p>
+                    <p>quibusdam cupiditate voluptatum</p>
+                </div>
+            </div>
+            `;
     }
 }
 
